a0c1d748df68c658b3f6506cd9066d1b9202f6c4
Felipe Allage <felipeallage@hotmail.com>
2022-01-12 13:41:24 -0300

    Objeto Completo

---
+++ Model/License.swift
@@ -0,0 +1,19 @@
+//
+//  License.swift
+//  GitRepoApp
+//
+//  Created by Admin on 1/12/22.
+//
+
+import Foundation
+
+class License: Codable {
+    
+    var key: String
+    var name: String
+    var spdx_id: String
+    var url: String?
+    var node_id: String
+}
+
+

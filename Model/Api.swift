8eba144957ec45c14b967b55e6c3a44a8235b33d
Felipe Allage <felipeallage@hotmail.com>
2022-01-17 20:49:15 -0300

    Api finalizada

---
+++ Model/Api.swift
@@ -0,0 +1,35 @@
+//
+//  Api.swift
+//  GitRepoApp
+//
+//  Created by Admin on 1/17/22.
+//
+
+import Foundation
+
+protocol RepositoryApiDelegate {
+    
+    func didGetRepository(repository: UserInfoContainer)
+    
+}
+
+struct RepositoryApi {
+    
+    var delegate: RepositoryApiDelegate?
+    
+    func getRepository() {
+        let link = "https://api.github.com/search/repositories?q=language&=swift&sort=stars"
+        guard let url = URL(string: link) else {
+            return
+        }
+        let request = URLRequest(url: url)
+        URLSession.shared.dataTask(with: request) {data, response, error in
+            if let data = data {
+                let repositoryList = try! JSONDecoder().decode(UserInfoContainer.self, from: data)
+                DispatchQueue.main.async {
+                    delegate?.didGetRepository(repository: repositoryList)
+                }
+            }
+        }.resume()
+    }
+}
